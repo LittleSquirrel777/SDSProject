d8e52056328a974b03c972ae0bf54f23d170d582
littlesquirrel <2436700441@qq.com>
2021-07-17 10:47:17 +0800

    base structure10

--- SDSProject/sds/src/main/java/com/iman/sds/controller/SensorController.java
+++ SDSProject/sds/src/main/java/com/iman/sds/controller/SensorController.java
@@ -23,7 +23,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 //参数（sensor，water）  上传传感器的一个数据到链上
 //private static String callContractDataQueryCredit(Sensor sensor)
 //参数（sensor）    根据传感器的信息，返回传感器对应的所有水数据
-
+// private static boolean callContractAddLogDataCredit(Sensor sensor, Score score, String operation, ScDescription scDescription)
+//增加一个日志数据  参数（sensor，score，操作符 “+” “-”，scDescription 描述）
+// private static String callContractQueryLogDataCredit(Sensor sensor)
+//调用合约差一条记录 参数（sensor）
 public class SensorController {
 
 }
