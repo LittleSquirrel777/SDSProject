b3aba52d580030de8bed52103ad8b81e110fdc29
AtZDD <3021151981@qq.com>
2021-07-19 02:21:31 +0800

    /api/list/score002

--- SDSProject/sds/src/main/java/com/iman/sds/controller/ScoreController.java
+++ SDSProject/sds/src/main/java/com/iman/sds/controller/ScoreController.java
@@ -2,6 +2,7 @@ package com.iman.sds.controller;
 
 import com.iman.sds.common.ResponseMsg;
 import com.iman.sds.entity.Score;
+import com.iman.sds.po.ListScoreParam;
 import com.iman.sds.service.ScoreService;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,9 +19,10 @@ public class ScoreController extends BaseController{
     @Autowired
     ScoreService scoreService;
 
-    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    @RequestMapping(value = "/list", method = RequestMethod.POST)
     @RequiresPermissions(value = { "AllScore:view" })
-    public ResponseMsg getAllScore(@RequestParam String factoryName){
+    public ResponseMsg getAllScore(@RequestBody ListScoreParam listScoreParam){
+        String factoryName = listScoreParam.getFactoryName();
         if (factoryName.length() == 0) {
             List<Score> list = scoreService.getScoreById();
             Map result = new HashMap();
---
+++ SDSProject/sds/src/main/java/com/iman/sds/po/ListScoreParam.java
@@ -0,0 +1,12 @@
+package com.iman.sds.po;
+
+import lombok.Data;
+import lombok.ToString;
+
+import java.util.Date;
+
+@Data
+@ToString
+public class ListScoreParam {
+    private String factoryName;
+}
